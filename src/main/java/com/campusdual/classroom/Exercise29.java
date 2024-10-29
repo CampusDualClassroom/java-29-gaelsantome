fcc2981249171227b8d8204d2b5543efff4044e7
Gael Santome Lopez <you@example.comgaelsantome@gmail.com git config --global user.name Gasel>
2024-10-29 16:52:56 +0100

    Ejercicio 29 finalizado

--- src/main/java/com/campusdual/classroom/Exercise29.java
+++ src/main/java/com/campusdual/classroom/Exercise29.java
@@ -3,5 +3,14 @@ package com.campusdual.classroom;
 public class Exercise29 {
     public static void main(String[] args) {
 
+        try {
+            throw new IllegalStateException("Excepción");
+        } catch (IllegalStateException e) {
+            System.out.println("Excepción: " + e.getMessage());
+        }
+
+        System.out.println("Programa finalizado");
     }
+
 }
+
